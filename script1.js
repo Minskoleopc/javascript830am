9561e98db637915a3aa0dbec18f222802fe51e89
unknown <Minskoleopc>
2024-06-17 20:01:53 +0530

    addded

---
+++ script1.js
@@ -0,0 +1,90 @@
+let a = 10 
+a = 100
+console.log(a)
+
+
+const h = 10
+console.log(h)
+//h = 5000
+
+// Arithmetic operation 
+
+let x = 10
+let y = 5 
+
+console.log(x+y)
+console.log(x-y)
+console.log(x*y)
+console.log(x/y)
+console.log(x%y)
+
+let q = 9
+let r = 3
+
+console.log(q+r)
+console.log(q-r)
+console.log(q*r)
+console.log(q/r)
+console.log(q%r)
+
+// function 
+
+function Calculator(x,y){
+    console.log(x+y)
+    console.log(x-y)
+    console.log(x*y)
+    console.log(x/y)
+    console.log(x%y)
+}
+Calculator(12,4)
+
+
+// function without parameter and without return 
+
+function add(){
+    console.log(9+7)
+}
+add()
+add()
+add()
+add()
+add()
+add()
+
+// function with parameter and with return type 
+function addB(x,y){
+    console.log(x+y)
+}
+addB(12,3)
+addB(12,31)
+addB(12,311)
+addB(12,3111)
+
+// function with parameter and with return type 
+
+function addC(x,y){
+    return x + y
+}
+let e = addC(12,4)
+console.log(e)
+console.log(e + e)
+console.log(e * 0.10)
+
+// Introver        // Extrovert
+
+// calm            // loud 
+// less outing     // more outing
+// less social     // more social
+
+// Human -- amol
+// Properties - age , color , height , weight , gender
+// Methods - talk() , walk()
+
+// Vehicle  - car
+// Properties - color , regno ,type , modelNo
+// Methods - start() , stop()
+
+
+// Backacc  icici bank 
+// Properties - accNo , accName , bal , IFSC code , branchName
+// Method - deposit(), withdrawl() 
