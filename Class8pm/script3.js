7b214c036a7e4deaacede3602d9bd4abcc01b0a3
unknown <Minskoleopc>
2024-06-17 20:33:05 +0530

    added

---
+++ Class8pm/script3.js
@@ -0,0 +1,49 @@
+let a  = 10
+console.log(a)
+
+// define an array 
+//             0  1  2  3  4
+let numbers = [11,22,33,44,55]
+//             0           1       2      3
+let names = ["chinmay","shirish","raj",'satish']
+
+let studentsGrade = [true,false,true,true]
+//            0          1            2      3   4
+let info = ["chinmay","deshpande",7709192441,23,true]
+
+// program 1
+//               0         1          2         3         4
+let country = ["India","Srilanka","Pakistan","China","Bhutan"]
+// retrive the value 
+console.log(country[0])
+console.log(country[1])
+
+// program 2
+//            0         1          2       3          4
+let city = ["nagpur","mumbai","kolkata","chennai","raipur"]
+// Object --- properties and methods
+console.log(city.length)
+console.log(city[0])
+
+for(let i = 0 ; i < 5 ; i++){ // 1 // 2 // 3 // 4 // 5
+    //console.log(i) // 0 // 1 // 2 // 3 // 4
+    console.log(city[i])
+}
+//             0         1       2       3
+let fruit = ["apple","mango","grapes","chikoo"]
+
+for(let i = 0 ; i < 4 ; i++){
+   // console.log(i)
+   console.log(fruit[i])
+}
+
+// program 3
+//                 0          1        2       3
+let vegetable = ["tomato","potato","brinjal","onion"]
+console.log(vegetable[0])
+for(let i = 0 ; i < 4; i++ ){
+    //console.log(i)
+    console.log(vegetable[i])
+}
+
+
