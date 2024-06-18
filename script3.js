1261b820f5461d5262e18f0c2bb9e760dbe36d1d
unknown <Minskoleopc>
2024-06-18 19:56:27 +0530

    added

---
+++ script3.js
@@ -0,0 +1,65 @@
+// entity < entity ---- comparison --- boolean ---- true or false
+
+// <, > 
+// <= , >= 
+// == , != --- value 
+// === , !== --- value and type 
+
+console.log(7 === 7)
+console.log(7 === '7')
+console.log(7 == '7')
+console.log(7 != '7')
+console.log(7 != 7)
+console.log(7 > 3)
+console.log(7 < 3)
+console.log(7 >= 3)
+console.log(7 <= 3)
+console.log(7 >= 7)
+console.log(7 <= 7)
+console.log(7 != 7)
+
+// logical operator 
+
+// && - AND 
+
+
+
+// true  &&   true   ---- true
+// false &&   true   ----  false
+// true  &&   false  ----  false
+// false &&   false  ----  false
+
+console.log(7 === 7 && 8 == 8)
+console.log(6 === '6' && 7 ===7)
+console.log(6 === 6 && 7 === '7')
+console.log(6 != 6 && 7 != '7')
+
+
+// || - OR
+// true  ||   true   ----- true 
+// true  ||   false  ----- true 
+// false ||   true   ----- true 
+// false ||   false  ----- false
+
+console.log(8 == 8 || 8 != 9)
+console.log(8 == 8 || 8 == 9)
+console.log(9 === '9' || 6 == 6)
+console.log(9 === '9' || 6 === '6')
+
+
+// ! - NOT
+// !true   ---- false
+// !false  ----- true
+
+console.log(!(8 === '8'))
+console.log(!(7 === 7))
+
+
+
+
+
+
+
+
+
+
