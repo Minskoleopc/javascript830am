9474e8f875617336f7d2432fec7c03343d22fa74
unknown <Minskoleopc>
2024-06-19 20:30:21 +0530

    added

---
+++ Class8pm/script5.js
@@ -0,0 +1,78 @@
+// Object 
+// Object --- properties and methods
+
+//Method 
+// Action and return type 
+
+// Gym()
+// action -  excercise
+// return -  health
+
+//            0       1         2      3
+let names = ["amol","rahul","sanket","ninad"]
+let q1 = names.length
+console.log(q1)
+
+// push()
+// action - add the element to the last
+// return - new length of array
+let q2 = names.push("nisha")
+console.log(q2)
+console.log(names)
+
+// unshift()
+// action - add the element at the start of array
+// return - new length of array
+
+let fruits = ["apple","mango","orange","banana"]
+let q3 = fruits.unshift("papaya")
+console.log(fruits)
+console.log(q3)
+
+
+// program 3
+// pop()
+// Action - removes the last element of array
+// Return - same element
+
+let vegetable = ["tomato","potato","brinjal","cauliflower"]
+let q4 = vegetable.pop()
+console.log(q4)
+console.log(vegetable)
+
+
+// program4
+// shift()
+// Action - removes the first element of array
+// Return - same element
+
+let numbers = [111,222,333,444]
+let q5 = numbers.shift()
+console.log(q5)
+console.log(numbers)
+
+
+// program 5
+// includes()
+// action - search the element inside array
+// return boolean --- true if found else false
+
+let cities = ["pune","banglore","kolkata","chennai"]
+let q6 = cities.includes("pune")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
