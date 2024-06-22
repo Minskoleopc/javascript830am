570e9ba5fc04ee68ac2e17e436f3d971e14987df
unknown <Minskoleopc>
2024-06-22 19:50:02 +0530

    added

---
+++ script10.js
@@ -0,0 +1,67 @@
+
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+
+// for loop and while loop 
+// for(intialization ; conditionCheck , increment/decrement){
+//     // statements
+// }
+
+
+// program 1 
+// i++ - increment the value of i by 1
+// i-- - decrement the value of i by 1
+
+
+for(let i = 1 ; i <= 3 ; i++){ //2 //3 // 4
+    console.log(i) // 1  // 2 // 3
+}
+
+//1 <= 3 --- true
+//2 <= 3 --- true
+//3 <= 3 --- true
+
+
+// program 1 to 5
+for(let i = 1 ; i <= 5 ; i++){ // 2 // 3 // 4 // 5 // 6
+    console.log(i) // 1 // 2 // 3 // 4  // 5
+}
+
+// program 3
+// print hello three times
+for(let i = 1 ; i <= 3 ; i++){ // 2 // 3 // 4
+    console.log("hello") // "hello" "hello","hello"
+}
+
+// program 4
+for(let i = 5 ; i >= 1 ;i--){ // 4 // 3 // 2 // 1 // 0
+    console.log(i) // 5 // 4 // 3 // 2 // 1
+}
+
+// program 5
+for(let i = 1 ; i <= 10 ; i++){
+    console.log(i * 2)
+}
+
+// program 6
+for(let i = 2 ; i <= 20 ; i = i + 2){ // 4 // 6 ----- 22
+    console.log(i) // 2 // 4 ------ 20
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
