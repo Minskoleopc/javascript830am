f766199239af8fea78a6aabef403905a2c6af828
unknown <Minskoleopc>
2024-06-29 08:58:22 +0530

    added

---
+++ script18.js
@@ -0,0 +1,14 @@
+
+// array
+
+let numbers = [11,22,33,44,55]
+let names = ["chinmay","sarika","poorva","abhisha"]
+let boolean  = [true , false , true , false ]
+let info = ["chinmay","deshpande",7709192441,true]
+
+
+// defining an array
+//              0       1        2           3
+let cities = ["pune","mumbai","bangalore","kolkata"]
+console.log(cities[0])
+console.log(cities[3])
