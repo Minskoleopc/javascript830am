f2ba1155202915454a459123a5324dbe979bc756
unknown <Minskoleopc>
2024-07-17 23:16:22 +0530

    added

---
+++ DOM3/script.js
@@ -0,0 +1,16 @@
+//<h2 id = "one" class = "three" name = "four">Minskole</h2>
+
+// selecting the element -- css selector
+
+let bytagName = document.querySelector('h2')
+console.log(bytagName)
+
+let byId = document.querySelector('#one')
+console.log(byId)
+
+let byClass = document.querySelector('.three')
+console.log(byClass)
+
+// tagName[att = "value"]
+let byAttr = document.querySelector('h2[name= "four"]')
+console.log(byAttr)
