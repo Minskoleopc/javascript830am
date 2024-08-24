fdfff7366d7a0159d336b0a8f4ace28a5fdf7b35
unknown <Minskoleopc>
2024-08-24 20:31:30 +0530

    added

--- OOps/script8.js
+++ OOps/script8.js
@@ -1,3 +1,7 @@
+// polymorphism 
+// inheritance 
+// encapsulation ----
+
 // program 1
 // class Human {   
 
@@ -50,7 +54,7 @@
 //     else if(typeof obj.bark === "function"){
 //         obj.bark()
 //     }
-   
+
 // }
 
 // let amol2 = new Human()
@@ -65,42 +69,98 @@
 
 // function declartion
 
-function addition(x,y){
+function addition(x, y) {
     return x + y
 }
 
-let a = addition(12,4)
+let a = addition(12, 4)
 console.log(a)
 
 // function expression 
 
-let addition2 = function(x,y){
+let addition2 = function (x, y) {
     return x + y
 }
-let a2 = addition2(23,4)
+let a2 = addition2(23, 4)
 console.log(a2)
 
 // arrow function
 
-let addition3 = (x,y)=>{
-    return x + y     
+let addition3 = (x, y) => {
+    return x + y
 }
-let q3 = addition3(23,4)
+let q3 = addition3(23, 4)
 console.log(q3)
 
 // if only 1 statement 
 
-let addition4 = (x,y) => x + y 
-let q4 = addition4(3,4)
+let addition4 = (x, y) => x + y
+let q4 = addition4(3, 4)
 console.log(q4)
 
 // if only one statement one parameter
-let addition5 = x => x + x 
+let addition5 = x => x + x
 let q5 = addition5(3)
 console.log(q5)
 
 // lexical scope
 
-// closures 
+function addition() {
+    let a = 10
+    let b = 20
+    console.log(a + b)
+
+    function addition2() {
+        let c = 8
+        let d = 4
+        console.log(a + b )
+
+        function addition3() {
+            let e = 19
+            console.log(a + b + c + d + e)
+        }
+        addition3()
+    }
+
+    addition2()
+}
+addition()
+
+// closures
+
+// function greet(){
+//     return function(){
+//         console.log("hello")
+//     }
+// }
+// let q1 = greet()
+// console.log(q1)
+
+// function addition(){
+//     console.log(9+9) // 18
+//     return 9+9
+//     console.log("bye")
+// }
+// let q2 = addition()
+// console.log(q2) // 18
+
+function additionD(){
+    let a = 10 
+    let b = 5
+    return function(){
+        console.log(a+b)
+    }
+}
+let r1 = additionD()
+
+// let r1 = function(){
+//     console.log(a+b)
+// }
+
+r1()
+
+
+
+
 
 // what is actual difference between arrow function and function expression
